2be644fe8444d0998454913f963ad22a70299262
ehickey08 <49757261+ehickey08@users.noreply.github.com>
2019-08-07 16:35:31 -0400

    Add apidoc and start commenting

--- api/server.js
+++ api/server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const helmet = require('helmet');
+const bodyParser = require('body-parser')
+const path = require('path')
 const usersRouter = require('./users/usersRouter');
 const postsRouter = require('./posts/postsRouter');
 const { logger } = require('./middleware');
+const apiDocsPath = path.join( __dirname, "../apidoc" );
 
 const server = express();
 
@@ -12,7 +15,7 @@ server.use(logger);
 server.use('/users', usersRouter);
 server.use('/posts', postsRouter);
 
-server.get('/', (req, res) => {
-    res.status(200).json({test: "Server is working"})
-});
+server.use( "/", express.static( apiDocsPath ) );
+server.use( bodyParser.urlencoded( { extended: true } ) );
+
 module.exports = server;
---
+++ apidoc/api_project.js
@@ -0,0 +1,20 @@
+define({
+  "name": "Web API 3 Challenge",
+  "version": "1.0.0",
+  "description": "API for a lambda project.",
+  "title": "Web 3 Api",
+  "url": "http://localhost:8001/",
+  "sampleUrl": "http://localhost:8001/",
+  "template": {
+    "withGenerator": true,
+    "withCompare": true
+  },
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2019-08-07T20:34:12.228Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.7"
+  }
+});
